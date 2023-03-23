66989f0b135231b6a878a44097637099644ad187
Alif Asraf <goodasraf@gmail.com>
2023-03-23 08:35:28 +0800

    remove unused code

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,17 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
 import ContactCard from "./ContactCard";
 import { Card } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
 const ContactList = (props) => {
   console.log(props);
-  const inputEl = useRef("")
+  const inputEl = useRef("");
   const renderContactList = props.contacts.map((contacts) => {
     return <ContactCard contacts={contacts} key={contacts.id} />;
   });
 
   const getSearhTerm = () => {
-      props.searchKeyword(inputEl.current.value);
-  }
+    props.searchKeyword(inputEl.current.value);
+  };
 
   return (
     <div style={{ paddingTop: "100px" }}>
@@ -25,12 +24,23 @@ const ContactList = (props) => {
       </h2>
       <div className="ui search">
         <div className="ui icon input">
-          <input ref={inputEl} type="text" placeholder="Search Contact" className="prompt" value={props.term} onChange={getSearhTerm} />
+          <input
+            ref={inputEl}
+            type="text"
+            placeholder="Search Contact"
+            className="prompt"
+            value={props.term}
+            onChange={getSearhTerm}
+          />
           <i className="search icon"></i>
         </div>
       </div>
       <br></br>
-      <Card.Group>{renderContactList.length > 0 ? renderContactList : "No contact available"}</Card.Group>
+      <Card.Group>
+        {renderContactList.length > 0
+          ? renderContactList
+          : "No contact available"}
+      </Card.Group>
     </div>
   );
 };
